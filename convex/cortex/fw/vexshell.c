3e379e2ddaf13ce2337037d1abec77b4b84e0b77
James Pearman <jbpearman@mac.com>
2014-01-04 15:55:21 -0800

    replace chprintf with vex_chprintf, add buffering for echoed characters
    Signed-off-by: James Pearman <jbpearman@mac.com>

--- convex/cortex/fw/vexshell.c
+++ convex/cortex/fw/vexshell.c
@@ -64,16 +64,9 @@
 
 #include <string.h>
 
-#include "ch.h"
-#include "hal.h"
-#include "chprintf.h"
-#include "vex.h"
-
-#if (CH_KERNEL_MAJOR >= 2) && (CH_KERNEL_MINOR >= 6)
-#define      vexStreamPut(stream, c)    chSequentialStreamPut( stream, c );
-#else
-#define      vexStreamPut(stream, c)    chIOPut( stream, c );
-#endif
+#include "ch.h"         // needs for all ChibiOS programs
+#include "hal.h"        // hardware abstraction layer header
+#include "vex.h"        // vex library header
 
 #define CR                    '\r'
 #define LF                    '\n'
@@ -88,6 +81,10 @@
 
 // Shell termination event source.
 EventSource shell_terminated;
+
+// local shell flags
+static  bool_t  shell_echo = true;
+static  bool_t  shell_bufr = true;
 
 // String to token
 static char *_strtok(char *str, const char *delim, char **saveptr)
@@ -113,7 +110,7 @@ static char *_strtok(char *str, const char *delim, char **saveptr)
 static void
 usage(vexStream *chp, char *p)
 {
-    chprintf(chp, "Usage: %s\r\n", p);
+    vex_chprintf(chp, "Usage: %s\r\n", p);
 }
 
 /*-----------------------------------------------------------------------------*/
@@ -123,7 +120,7 @@ list_commands(vexStream *chp, const ShellCommand *scp)
 {
     while (scp->sc_name != NULL)
         {
-        chprintf(chp, "%s ", scp->sc_name);
+        vex_chprintf(chp, "%s ", scp->sc_name);
         scp++;
         }
 }
@@ -141,29 +138,29 @@ cmd_info(vexStream *chp, int argc, char *argv[])
         return;
     }
 
-    chprintf(chp, "Kernel:       %s\r\n", CH_KERNEL_VERSION);
+    vex_chprintf(chp, "Kernel:       %s\r\n", CH_KERNEL_VERSION);
 #ifdef CH_COMPILER_NAME
-    chprintf(chp, "Compiler:     %s\r\n", CH_COMPILER_NAME);
+    vex_chprintf(chp, "Compiler:     %s\r\n", CH_COMPILER_NAME);
 #endif
-    chprintf(chp, "Architecture: %s\r\n", CH_ARCHITECTURE_NAME);
+    vex_chprintf(chp, "Architecture: %s\r\n", CH_ARCHITECTURE_NAME);
 #ifdef CH_CORE_VARIANT_NAME
-    chprintf(chp, "Core Variant: %s\r\n", CH_CORE_VARIANT_NAME);
+    vex_chprintf(chp, "Core Variant: %s\r\n", CH_CORE_VARIANT_NAME);
 #endif
 #ifdef CH_PORT_INFO
-    chprintf(chp, "Port Info:    %s\r\n", CH_PORT_INFO);
+    vex_chprintf(chp, "Port Info:    %s\r\n", CH_PORT_INFO);
 #endif
 #ifdef PLATFORM_NAME
-    chprintf(chp, "Platform:     %s\r\n", PLATFORM_NAME);
+    vex_chprintf(chp, "Platform:     %s\r\n", PLATFORM_NAME);
 #endif
 #ifdef BOARD_NAME
-    chprintf(chp, "Board:        %s\r\n", BOARD_NAME);
+    vex_chprintf(chp, "Board:        %s\r\n", BOARD_NAME);
 #endif
 #ifdef CONVEX_VERSION
-    chprintf(chp, "ConVEX:       %s\r\n", CONVEX_VERSION);
+    vex_chprintf(chp, "ConVEX:       %s\r\n", CONVEX_VERSION);
 #endif
 #ifdef __DATE__
 #ifdef __TIME__
-    chprintf(chp, "Build time:   %s%s%s\r\n", __DATE__, " - ", __TIME__);
+    vex_chprintf(chp, "Build time:   %s%s%s\r\n", __DATE__, " - ", __TIME__);
 #endif
 #endif
 }
@@ -178,7 +175,7 @@ cmd_systime(vexStream *chp, int argc, char *argv[])
         usage(chp, "systime");
         return;
     }
-    chprintf(chp, "%lu\r\n", (unsigned long)chTimeNow());
+    vex_chprintf(chp, "%lu\r\n", (unsigned long)chTimeNow());
 }
 
 /*-----------------------------------------------------------------------------*/
@@ -198,17 +195,17 @@ cmd_mem(vexStream *chp, int argc, char *argv[])
     (void)argv;
     if (argc > 0)
         {
-        chprintf(chp, "Usage: mem\r\n");
+        vex_chprintf(chp, "Usage: mem\r\n");
         return;
         }
 
     n = chHeapStatus(NULL, &size);
-    chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
-    chprintf(chp, "heap fragments   : %u\r\n", n);
-    chprintf(chp, "heap free total  : %u bytes\r\n", size);
+    vex_chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
+    vex_chprintf(chp, "heap fragments   : %u\r\n", n);
+    vex_chprintf(chp, "heap free total  : %u bytes\r\n", size);
 
     flash = (SYMVAL(_textdata) - 0x08000000) + (SYMVAL(_edata) - SYMVAL(_data));
-    chprintf(chp, "flash used       : %lu (0x%05X) bytes\r\n", flash, flash);
+    vex_chprintf(chp, "flash used       : %lu (0x%05X) bytes\r\n", flash, flash);
 }
 
 /*-----------------------------------------------------------------------------*/
@@ -222,21 +219,21 @@ cmd_threads(vexStream *chp, int argc, char *argv[])
     (void)argv;
     if (argc > 0)
         {
-        chprintf(chp, "Usage: threads\r\n");
+        vex_chprintf(chp, "Usage: threads\r\n");
         return;
         }
 
-    chprintf(chp, "            name addr     stack    prio refs     state time\r\n");
+    vex_chprintf(chp, "            name addr     stack    prio refs     state time\r\n");
     tp = chRegFirstThread();
 
     do
         {
         if( tp->p_name != NULL )
-            chprintf(chp, "%16s ", tp->p_name);
+            vex_chprintf(chp, "%16s ", tp->p_name);
         else
-            chprintf(chp,"                 ");
+            vex_chprintf(chp,"                 ");
 
-        chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
+        vex_chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
                 (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
                 (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
                 states[tp->p_state], (uint32_t)tp->p_time);
@@ -252,12 +249,45 @@ cmd_reboot(vexStream *chp, int argc, char *argv[])
 {
     (void)argv;
     if (argc > 0) {
-        chprintf(chp, "Usage: rboot\r\n");
+        vex_chprintf(chp, "Usage: reboot\r\n");
         return;
     }
-    chprintf(chp, "rebooting...\r\n");
+    vex_chprintf(chp, "rebooting...\r\n");
     chThdSleepMilliseconds(100);
     NVIC_SystemReset();
+}
+
+/*-----------------------------------------------------------------------------*/
+static void
+cmd_flags(vexStream *chp, int argc, char *argv[])
+{
+    if (argc < 2) {
+        vex_chprintf(chp, "Usage:flags [flag] [on|off]\r\n");
+        vex_chprintf(chp, "      example\r\n");
+        vex_chprintf(chp, "      flags echo on\r\n");
+        return;
+    }
+
+    if(strcasecmp(argv[0],"echo")==0) {
+        if(strcasecmp(argv[1],"on")==0) {
+            shell_echo = true;
+            vex_chprintf(chp, "echo is on\r\n");
+        }
+        if(strcasecmp(argv[1],"off")==0) {
+            shell_echo = false;
+            vex_chprintf(chp, "echo is off\r\n");
+        }
+    }
+    if(strcasecmp(argv[0],"bufr")==0) {
+        if(strcasecmp(argv[1],"on")==0) {
+            shell_bufr = true;
+            vex_chprintf(chp, "buffering is on\r\n");
+        }
+        if(strcasecmp(argv[1],"off")==0) {
+            shell_bufr = false;
+            vex_chprintf(chp, "buffering is off\r\n");
+        }
+    }
 }
 
 /*-----------------------------------------------------------------------------*/
@@ -269,6 +299,7 @@ static ShellCommand local_commands[] = {
   {"mem",     cmd_mem},
   {"threads", cmd_threads},
   {"reboot",  cmd_reboot},
+  {"flags",   cmd_flags},
   {NULL, NULL}
 };
 
@@ -294,7 +325,7 @@ cmdexec(const ShellCommand *scp, vexStream *chp, char *name, int argc, char *arg
 /*                                                                             */
 /*  Shell thread function.                                                     */
 /*                                                                             */
-/*  p - pointer to a vexStream object                                        */
+/*  p - pointer to a vexStream object                                          */
 /*  Termination reason.                                                        */
 /*    terminated by command.                                                   */
 /*    terminated by reset condition on the I/O channel.                        */
@@ -312,21 +343,21 @@ shell_thread(void *p)
     char *args[SHELL_MAX_ARGUMENTS + 1];
 
     chRegSetThreadName("shell");
-    chprintf(chp, "\033[2J"); // clearscreen
-    chprintf(chp, "\r\nVEX-Cortex Shell V1.00\r\n");
-    chprintf(chp, "Kernel:%s  ConVEX:%s  ", CH_KERNEL_VERSION, CONVEX_VERSION);
+    vex_chprintf(chp, "\033[2J"); // clearscreen
+    vex_chprintf(chp, "\r\nVEX-Cortex Shell V1.00\r\n");
+    vex_chprintf(chp, "Kernel:%s  ConVEX:%s  ", CH_KERNEL_VERSION, CONVEX_VERSION);
 #ifdef __DATE__
 #ifdef __TIME__
-    chprintf(chp, "Build time:%s%s%s\r\n", __DATE__, " - ", __TIME__);
+    vex_chprintf(chp, "Build time:%s%s%s\r\n", __DATE__, " - ", __TIME__);
 #endif
 #endif
 
     while (TRUE)
         {
-        chprintf(chp, "ch> ");
+        vex_chprintf(chp, "ch> ");
         if (shellGetLine(chp, line, sizeof(line)))
             {
-            chprintf(chp, "\r\nlogout");
+            vex_chprintf(chp, "\r\nlogout");
             break;
             }
 
@@ -339,7 +370,7 @@ shell_thread(void *p)
             {
             if (n >= SHELL_MAX_ARGUMENTS)
                 {
-                chprintf(chp, "too many arguments\r\n");
+                vex_chprintf(chp, "too many arguments\r\n");
                 cmd = NULL;
                 break;
                 }
@@ -367,17 +398,17 @@ shell_thread(void *p)
                     continue;
                     }
 
-                chprintf(chp, "Commands: help exit ");
+                vex_chprintf(chp, "Commands: help exit ");
                 list_commands(chp, local_commands);
                 if (scp != NULL)
                     list_commands(chp, scp);
-                chprintf(chp, "\r\n");
+                vex_chprintf(chp, "\r\n");
                 }
             else
             if (cmdexec(local_commands, chp, cmd, n, args) && ((scp == NULL) || cmdexec(scp, chp, cmd, n, args)))
                 {
-                chprintf(chp, "%s", cmd);
-                chprintf(chp, " ?\r\n");
+                vex_chprintf(chp, "%s", cmd);
+                vex_chprintf(chp, " ?\r\n");
                 }
             }
         }
@@ -392,9 +423,7 @@ shell_thread(void *p)
 }
 
 /*-----------------------------------------------------------------------------*/
-/*                                                                             */
-/*  Shell manager initialization.                                              */
-/*                                                                             */
+/** @brief  Shell manager initialization.                                      */
 /*-----------------------------------------------------------------------------*/
 
 void shellInit(void)
@@ -403,18 +432,16 @@ void shellInit(void)
 }
 
 /*-----------------------------------------------------------------------------*/
-/*                                                                             */
-/*  Spawns a new shell.                                                        */
-/*  CH_USE_MALLOC_HEAP and @p CH_USE_DYNAMIC must be enabled.                  */
-/*  scp          pointer to a @p ShellConfig object                            */
-/*  size         size of the shell working area to be allocated                */
-/*  prio         priority level for the new shell                              */
-/*  return       A pointer to the shell thread.                                */
-/*  retval NULL  thread creation failed because memory allocation.             */
-/*                                                                             */
+/**  @brief  Spawns a new shell using dynamic memory allocation                */
+/**  @param[in] scp  pointer to a @p ShellConfig object                        */
+/**  @param[in] size size of the shell working area to be allocated            */
+/**  @param[in] prio priority level for the new shell                          */
+/**  @returns   A pointer to the shell thread.                                 */
+/**  @retval NULL  thread creation failed because memory allocation.           */
+/**  @note CH_USE_MALLOC_HEAP and @p CH_USE_DYNAMIC must be enabled.           */
 /*-----------------------------------------------------------------------------*/
 
-#if CH_USE_HEAP && CH_USE_DYNAMIC
+#if (CH_USE_HEAP && CH_USE_DYNAMIC) || defined(__DOXYGEN__)
 Thread *shellCreate(const ShellConfig *scp, size_t size, tprio_t prio)
 {
     return chThdCreateFromHeap(NULL, size, prio, shell_thread, (void *)scp);
@@ -422,13 +449,13 @@ Thread *shellCreate(const ShellConfig *scp, size_t size, tprio_t prio)
 #endif
 
 /*-----------------------------------------------------------------------------*/
-/*                                                                             */
-/*  Create statically allocated shell thread.                                  */
-/*  scp          pointer to a @p ShellConfig object                            */
-/*  size         size of the shell working area to be allocated                */
-/*  prio         priority level for the new shell                              */
-/*  return       A pointer to the shell thread.                                */
-/*                                                                             */
+/**  @brief  Create statically allocated shell thread.                         */
+/**  @param[in] scp  pointer to a @p ShellConfig object                        */
+/**  @param[in] wsp  pointer to a working area dedicated to the shell thread   */
+/**                  stack                                                     */
+/**  @param[in] size size of the shell working area to be allocated            */
+/**  @param[in] prio priority level for the new shell                          */
+/**  @returns   A pointer to the shell thread.                                 */
 /*-----------------------------------------------------------------------------*/
 
 Thread *shellCreateStatic(const ShellConfig *scp, void *wsp, size_t size, tprio_t prio)
@@ -437,9 +464,12 @@ Thread *shellCreateStatic(const ShellConfig *scp, void *wsp, size_t size, tprio_
 }
 
 /*-----------------------------------------------------------------------------*/
-/*                                                                             */
-/*  Gets a single line of input                                                */
-/*                                                                             */
+/** @private                                                                   */
+/** @brief Gets a single line of input for the shell                           */
+/** @param[in]  chp     A pointer to a vexStream object                        */
+/** @param[in]  line    A pointer the inpout buffer                            */
+/** @param[in]  size    size of the inpout buffer                              */
+/** @returns    True if shell is to be terminated else false                   */
 /*-----------------------------------------------------------------------------*/
 
 bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
@@ -449,6 +479,8 @@ bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
     short escSeq = 0;
     char *p = line;
     char *q;
+    char *r = line;
+    systime_t   lasttime = chTimeNow();
 
     // not in escape sequence
     escSeq = 0;
@@ -483,12 +515,15 @@ bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
             switch(c)
                 {
                 case 'A':   // Cursor Up
-                    chprintf(chp, "%s", history);
+                    if( shell_echo )
+                        vex_chprintf(chp, "%s", history);
 
                     // copy history buffer to line
                     q = history;
                     while((p < line + size - 1) && (*q != 0))
                         *p++ = *q++;
+                    // assume no buffering for history
+                    r = p;
                     break;
 
                 default:
@@ -503,7 +538,7 @@ bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
             {
             // CTRL D quits shell
             case    CTRL_D:
-                chprintf(chp, "^D");
+                vex_chprintf(chp, "^D");
                 return TRUE;
                 break;
 
@@ -512,22 +547,37 @@ bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
             case    0x7F:       // for some reason OSX screen outputs 7F
                 if (p != line)
                     {
-                    vexStreamPut(chp, (uint8_t)BKSPACE_CHAR);
-                    vexStreamPut(chp, 0x20);
-                    vexStreamPut(chp, (uint8_t)BKSPACE_CHAR);
+                    if( shell_echo ) {
+                        // print any buffered characters
+                        while( r != p )
+                            vex_chprintf(chp, "%c", (uint8_t)*r++);
+                        // print backspace and wipe the previous character
+                        vex_chprintf(chp, "%c %c", (uint8_t)BKSPACE_CHAR, (uint8_t)BKSPACE_CHAR );
+                        }
+
+                    // one less character
                     p--;
+                    // no more buffering
+                    r = p;
                     }
                 break;
 
             case    CR:
-                chprintf(chp, "\r\n");
                 *p = 0;
 
-                // copy to history buffer
-                for( q=history, p=line;*p!=0;q++,p++)
-                    *q = *p;
-                *q = 0;
+                if(shell_echo) {
+                    // print any remaining chars that came in quickly
+                    if( r != p )
+                        vex_chprintf(chp, "%s", r);
 
+                    // copy to history buffer
+                    for( q=history, p=line;*p!=0;q++,p++)
+                        *q = *p;
+                    *q = 0;
+                    }
+
+                // send cr/lf
+                vex_chprintf(chp, "\r\n");
                 return FALSE;
                 break;
 
@@ -552,8 +602,16 @@ bool_t shellGetLine( vexStream *chp, char *line, unsigned size )
             default:
                 if (c >= 0x20) {
                     if (p < line + size - 1) {
-                        vexStreamPut(chp, (uint8_t)c);
                         *p++ = (char)c;
+
+                        if( shell_echo ) {
+                            if( !shell_bufr || (chTimeNow() - lasttime) > 50 ) {
+                                while( r != p )
+                                    vex_chprintf(chp, "%c", (uint8_t)*r++);
+                            }
+
+                        lasttime = chTimeNow();
+                        }
                     }
                 }
                 break;
